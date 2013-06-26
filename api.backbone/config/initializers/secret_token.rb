18e73fdfdf5f890c233454172057ea4e8589f527
Vinicius da Costa Pires <vinicius.costa.pires@gmail.com>
2013-06-26 00:36:44 -0300

    Adicionando o servidor REST e o começo do código para a parte 5 do tutorial

---
+++ api.backbone/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ApiBackbone::Application.config.secret_token = 'dc70d8ca5121923f33ce01530882f41b651773ab17763f25478cc8db4d2665d6a88239050ccf86fc879792f921e9900723022177abed8632580d69aa9c71a6fe'
--- introduction/js/app.js
+++ introduction/js/app.js
@@ -59,7 +59,7 @@ var View = Backbone.View.extend({
 var view = new View({model:model});
 */
 
-/* -- Code for Part 3 */
+/* -- Code for Part 3
 var Router = Backbone.Router.extend({
     routes:{
         "foo/:bar":"paramtest",
@@ -76,3 +76,82 @@ var Router = Backbone.Router.extend({
 new Router();
 
 Backbone.history.start();
+*/
+
+/* -- Code for part 4
+var Person = Backbone.Model.extend({
+    initialize:function(){
+        console.log('person init');
+    },
+    defaults: {
+        name:'unknown',
+        age:'unknown',
+        id:0
+    }
+});
+
+var People = Backbone.Collection.extend({
+    initialize:function(){
+        console.log('people collection init');
+    },
+    model:Person,
+    // o método add ordena pelo comparador
+    comparator:function(person1, person2){
+        return person1.get('name') > person2.get('name');
+    }
+});
+
+var person = new Person({name:'Pires'});
+
+var people = new People(person);
+
+people.add([{name:'Bob'},{name:"Jim"}]);
+console.log(people.toJSON());
+
+people.remove(person);
+console.log(people.toJSON());
+
+people.reset([{name:'Bob'},{name:'Pires',id:3}]);
+console.log(people.toJSON());
+
+console.log(people.get(3)); // Busca pelo id do servidor
+console.log(people.get('c0')); //Busca pelo id do Cliente
+console.log(people.at(1)); // Busca pelo índice da lista
+
+// people.push, people.pop, people.shift, people.unshift
+
+people.sort();  // Se precisar reordenar a collection, por usar push-pop ou editar alguma propriedade que é usada na ordenação
+
+console.log( people.pluck('name') ); // Retorna um array apenas com o campo especificado
+
+console.log( people.where({name:'Bob'}) ); // Busca pela propriedade informada
+*/
+
+/* -- Code for part 5 */
+
+$.ajaxPrefilter(function(options, originalOptions, jqXhr) {
+    options.url = 'http://localhost:3000' + options.url;
+});
+
+var Person = Backbone.Model.extend({
+    initialize:function(){
+        this.on('all', function(e){
+            console.log(this.get('name')+' event: ' + e);
+        });
+    },
+    defaults:{
+        name:'unknown',
+        age:'unknown'
+    },
+    url:'/people'
+});
+
+var People = Backbone.Collection.extend({
+    initialize:function(){
+        this.on('all', function(e){
+            console.log('People Event: ' + e );
+        });
+    },
+    model:Person
+});
+
